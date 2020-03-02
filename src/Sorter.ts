959547ca1fc3170f025cd7f8e4f7672d528a5ad5
Junjie Chen <junjie.chen18@gmail.com>
2020-03-01 19:36:28 -0500

    feat(bubble sort): define abstract swap method

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,4 +1,5 @@
 export abstract class Sorter {
   abstract length: number;
   abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
 }
