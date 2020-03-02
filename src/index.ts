1bcc6d351c481b3905238a5e0d3eff0c9f1225a1
Junjie Chen <junjie.chen18@gmail.com>
2020-03-01 20:02:46 -0500

    feat(log): assign instance of String class to string variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Array } from './Array';
+import { String } from './String';
 
 const array = new Array([10, 5, 0, 0, -5, -10]);
 
 array.sort();
 
 console.log(array.numbers);
+
+const string = new String('ZyXwVu');
