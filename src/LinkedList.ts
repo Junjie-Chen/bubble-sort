99fbdaf085e6cfd77e083bc5659c3a598e98bd2a
Junjie Chen <junjie.chen18@gmail.com>
2020-03-01 19:50:36 -0500

    feat(linked list): define add method

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -8,4 +8,21 @@ class Node {
 
 export class LinkedList extends Sorter {
   head: Node | null = null;
+
+  add(value: number): void {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let tail = this.head;
+
+      while (tail.next) {
+        tail = tail.next;
+      }
+
+      tail.next = node;
+      tail = node;
+    }
+  }
 }
