816667e95eba97a181fe7390bb956eea892a6556
Junjie Chen <junjie.chen18@gmail.com>
2020-03-01 19:41:16 -0500

    feat(array): define compare method

--- src/Array.ts
+++ src/Array.ts
@@ -8,4 +8,11 @@ export class Array extends Sorter {
   get length(): number {
     return this.numbers.length;
   }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    const leftNumber = this.numbers[leftIndex];
+    const rightNumber = this.numbers[rightIndex];
+
+    return leftNumber > rightNumber;
+  }
 }
