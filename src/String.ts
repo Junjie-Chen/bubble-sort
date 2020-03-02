01747f3fb6cf88083ed8a2cd65fab4219f0df7e0
Junjie Chen <junjie.chen18@gmail.com>
2020-03-01 19:45:09 -0500

    feat(string): define compare method

--- src/String.ts
+++ src/String.ts
@@ -8,4 +8,11 @@ export class String extends Sorter {
   get length(): number {
     return this.letters.length;
   }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    const leftLetter = this.letters[leftIndex];
+    const rightLetter = this.letters[rightIndex];
+
+    return leftLetter.toLowerCase() > rightLetter.toLowerCase();
+  }
 }
